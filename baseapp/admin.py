39c6554e878840ddbff382c3abcc462414714583
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-07-23 21:39:36 +0200

    Register category, company and customer to admin

--- baseapp/admin.py
+++ baseapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_per_page = 10
+
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'registered_on', 'registration_status']
+    list_per_page = 10
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
