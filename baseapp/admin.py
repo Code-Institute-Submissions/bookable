12be7057414319ed9ccb0920f30009fc57375d0d
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-07-24 06:47:21 +0200

    Add code to target companys in category on companys changelist

--- baseapp/admin.py
+++ baseapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from . import models
@@ -6,8 +7,27 @@ from . import models
 # Register your models here.
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'companys_count']
     list_per_page = 10
+
+    @admin.display(ordering='companys_count')
+    def companys_count(self, category):
+        url = (
+            reverse('admin:baseapp_company_changelist')
+            + '?'
+            + urlencode({
+                'category__id': str(category.id)
+            }))
+        return format_html(
+                '<a href="{}">{}</a>',
+                url,
+                category.companys_count
+                )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            companys_count=Count('company')
+        )
 
 
 @admin.register(models.Company)
@@ -20,7 +40,7 @@ class CompanyAdmin(admin.ModelAdmin):
             reverse('admin:auth_user_changelist')
             + '?'
             + urlencode({
-                'q': str(company.company_name)
+                'company__id': str(company.id)
             }))
         return format_html(
             '<a href="{}">{}</a>',
