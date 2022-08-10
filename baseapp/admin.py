6205f3e6e82f4632678bc1f6b2fe354b0a248ff0
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-10 23:58:18 +0200

    Add code to unregister allauth from admin view

--- baseapp/admin.py
+++ baseapp/admin.py
@@ -1,15 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.sites.models import Site
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
+from allauth.socialaccount.models import (
+    EmailAddress, SocialToken, SocialAccount, SocialApp
+    )
+from core.models import CustomUser
 from . import models
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UserAdmin(BaseUserAdmin):
+    list_per_page = 10
+    search_fields = ['username']
+    add_fieldsets = (
+        (None, {
+                "classes": ("wide",),
+                "fields": (
+                    "first_name", "last_name",
+                    "username", "email",
+                    "password1", "password2",
+                    ),
+            },),
+    )
+
+
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'companys_count']
     list_per_page = 10
     search_fields = ['title']
+    fields = ['title']
 
     @admin.display(ordering='companys_count')
     def companys_count(self, category):
@@ -43,10 +66,9 @@ class CompanyAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['company_name__istartswith']
 
-
     def user_id(self, company):
         url = (
-            reverse('admin:auth_user_changelist')
+            reverse('admin:core_customuser_changelist')
             + '?'
             + urlencode({
                 'company__id': str(company.id)
@@ -63,3 +85,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
+admin.site.unregister(EmailAddress)
+admin.site.unregister(Site)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialApp)
