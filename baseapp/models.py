b03505de133185d669a6b3a9764708805d6ce0cd
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-11 00:00:36 +0200

    Remove offer model & refactor models

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,9 +1,8 @@
+from django.conf import settings
 from django.core.validators import URLValidator
 from django.db import models
-from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 
 REGISTRATION_STATUS = ((0, 'Pending'), (1, 'Approved'), (2, 'Disapproved'))
 
@@ -12,6 +11,7 @@ class Category(models.Model):
     company_in_category = models.ForeignKey(
         'Company',
         on_delete=models.SET_NULL,
+        blank=True,
         null=True,
         related_name='+'
         )
@@ -23,27 +23,16 @@ class Category(models.Model):
         ordering = ['title']
 
 class Company(models.Model):
-    SPOT_TYPE_SEATS = 'SE'
-    SPOT_TYPE_SPOTS = 'SP'
-    SPOT_CHOICES = [
-      (SPOT_TYPE_SEATS, 'SEATS'),
-      (SPOT_TYPE_SPOTS, 'SPOTS'),
-    ]
     brand_image = CloudinaryField('image/logo', default='placeholder')
     company_name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
     google_map = models.CharField(max_length=255, validators=[URLValidator()])
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     website = models.CharField(max_length=255, validators=[URLValidator()])
-    spot_type = models.CharField(
-        max_length=2,
-        choices=SPOT_CHOICES,
-        default=SPOT_TYPE_SEATS
-        )
-    spots = models.PositiveIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    spots = models.PositiveIntegerField('How many spots or seats?')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
     registered_on = models.DateTimeField(auto_now_add=True)
     registration_status = models.IntegerField(choices=REGISTRATION_STATUS, default=0)
 
@@ -59,20 +48,10 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     company = models.OneToOneField(Company, on_delete=models.CASCADE, primary_key=True)
 
+
 class Image(models.Model):
     image = CloudinaryField('image', default='placeholder')
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-
-
-class Offer(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    offer_price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
-    last_update = models.DateTimeField(auto_now_add=True)
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
-
-    def __str__(self) -> str:
-        return self.title
 
 
 class Customer(models.Model):
@@ -103,7 +82,12 @@ class Booking(models.Model):
         )
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-    # booking_item = models.ForeignKey('BookingItem', on_delete=models.PROTECT, related_name='+')
+
+    class Meta:
+        permissions = [
+                ('reject_booking', 'Can reject booking'),
+                ('accept_booking', 'Can accept booking'),
+            ]
 
     def __str__(self) -> str:
         return f'Booking Number {self.id}'
@@ -111,9 +95,7 @@ class Booking(models.Model):
 
 class BookingItem(models.Model):
     booking = models.ForeignKey(Booking, on_delete=models.PROTECT)
-    offer = models.ForeignKey(Offer, on_delete=models.PROTECT)
     spot_type_quantity = models.PositiveSmallIntegerField()
-    offer_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self) -> str:
         return f'Booking Item Number {self.id}'
