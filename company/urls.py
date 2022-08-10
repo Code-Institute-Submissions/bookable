a386ddec760dc5bfaeedbd750050d8eb968fd1c3
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-11 00:12:08 +0200

    Add company index path

---
+++ company/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from .views import *
+
+
+urlpatterns = [
+    path(
+        '',
+        CompanyView.as_view(),
+        name='index'
+    ),
+]
