644c9035d2959aa80b0b66ee4a531013791a1a3c
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-11 10:39:48 +0200

    Add fix for redirection after company form submission

--- company/views.py
+++ company/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
 from core.models import CustomUser
 from baseapp.models import Booking, Company, Address
 from .forms import NewCompanyForm, CompanyAddressForm
@@ -11,10 +13,15 @@ class CompanyView(View):
     def get(self, request):
         if request.user.is_authenticated:
             try:
-                if Company.objects.filter(user_id=request.user.id):
+                queryset = Company.objects.get(user_id=request.user.id)
+                if queryset:
+                    p = Paginator(Booking.objects.filter(company_id=request.user.id), 10)
+                    page = request.GET.get('page')
+                    index = p.get_page(page)
                     return render(
                         request,
-                        'company/index.html'
+                        'company/index.html',
+                        { 'index': index }
                         )
             except ObjectDoesNotExist:
                 return render(
@@ -26,28 +33,33 @@ class CompanyView(View):
                     }
                 )
 
-    def post(self, request, **kwargs):
+    def post(self, request, *args, **kwargs):
         """Post New Company Details"""
         user = get_object_or_404(CustomUser, pk=request.user.id)
         if request.user.is_authenticated:
             form_company = NewCompanyForm(request.POST)
             form_company_address = CompanyAddressForm(request.POST)
             if form_company.is_valid() and form_company_address.is_valid():
+                slug = str(form_company.save(commit=False)).replace(' ', '-').replace("'", '').lower()
                 form = Company.objects.create(
                     user_id=user.id,
+                    slug=slug,
                     **form_company.cleaned_data
                     )
                 new_company = Company.objects.get(company_name=form.company_name)
+                print(new_company.id)
                 Address.objects.create(
                     pk=new_company.id,
                     **form_company_address.cleaned_data
                     )
-                context = {
+                return HttpResponseRedirect(
+                        reverse('company_index')
+                    )
+            return render(
+                request,
+                'company/index.html',
+                {
                     "form_company": form_company,
                     "form_address": form_company_address,
                 }
-                return render(
-                  request,
-                  'company/index.html',
-                  context
-                )
+            )
