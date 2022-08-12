9669a0b393f5fc1b27390b12efbc5982a1a3d70e
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-12 20:32:47 +0200

    Add company delete view & refactor code

--- company/views.py
+++ company/views.py
@@ -3,8 +3,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
-from core.models import CustomUser
 from baseapp.models import Booking, Company, Address
+from core.models import CustomUser
 from .forms import CompanyForm, CompanyAddressForm
 
 
@@ -15,15 +15,33 @@ class CompanyAccountView(View):
     def get(self, request):
         if request.user.is_authenticated:
             try:
-                queryset = Company.objects.get(user_id=request.user.id)
+                queryset = Company.objects.select_related('user').get(user_id=request.user.id)
+                print(queryset.user.first_name)
                 if queryset:
-                    p = Paginator(Booking.objects.filter(company_id=queryset.id), 10)
-                    page = request.GET.get('page')
-                    account = p.get_page(page)
+                    context = {
+                        "company": queryset.company_name,
+                        "status": queryset.registration_status,
+                        "user": queryset.user,
+                    }
+                    if queryset.registration_status == 'Approved':
+                        p = Paginator(Booking.objects.filter(company_id=queryset.id), 10)
+                        page = request.GET.get('page')
+                        account = p.get_page(page)
+                        return render(
+                            request,
+                            'company/account.html',
+                            { "page": account, "context": context }
+                            )
+                    elif queryset.registration_status == 'Disapproved':
+                        return render(
+                            request,
+                            'company/inactive_company.html',
+                            context
+                            )
                     return render(
                         request,
-                        'company/account.html',
-                        { 'page': account }
+                        'company/pending_company.html',
+                        context
                         )
             except ObjectDoesNotExist:
                 return HttpResponseRedirect(
@@ -33,16 +51,17 @@ class CompanyAccountView(View):
                 request,
                 'company/add_company.html',
                 {
+                    "user": queryset.user,
                     "company_form": CompanyForm(),
                     "address_form": CompanyAddressForm(),
                 }
-            )
+                )
         return HttpResponseRedirect(
             reverse('home')
             )
 
 
-class CompanyAddView(View):
+class CompanyCreateView(View):
     """Add New Company View"""
     def get(self, request):
         """Get New Company Form"""
@@ -58,8 +77,9 @@ class CompanyAddView(View):
                     request,
                     'company/add_company.html',
                     {
+                        "user": queryset.user,
                         "company_form": CompanyForm(),
-                        "address_form": CompanyAddressForm()
+                        "address_form": CompanyAddressForm(),
                     }
                 )
 
@@ -70,13 +90,19 @@ class CompanyAddView(View):
             form_company = CompanyForm(request.POST)
             form_company_address = CompanyAddressForm(request.POST)
             if form_company.is_valid() and form_company_address.is_valid():
-                slug = request.POST.get('company_name').replace(' ', '-').replace("'", '').lower()
+                slug = request.POST \
+                    .get('company_name') \
+                    .replace(' ', '-') \
+                    .replace("'", '') \
+                    .lower()
                 form = Company.objects.create(
                     user_id=user.id,
                     slug=slug,
                     **form_company.cleaned_data
                     )
-                created_company = Company.objects.get(company_name=form.company_name)
+                created_company = Company.objects.get(
+                    company_name=form.company_name
+                    )
                 Address.objects.create(
                     pk=created_company.id,
                     **form_company_address.cleaned_data
@@ -89,12 +115,12 @@ class CompanyAddView(View):
                 'company/account.html',
                 {
                     "form_company": form_company,
-                    "form_address": form_company_address,
+                    "form_address": form_company_address
                 }
                 )
 
 
-class CompanyEditView(View):
+class CompanyUpdateView(View):
     """Edit Company View"""
     def get(self, request):
         if request.user.is_authenticated:
@@ -104,6 +130,8 @@ class CompanyEditView(View):
                 request,
                 'company/edit_company.html',
                 {
+                    "company": company,
+                    "user": company.user,
                     "company_form": CompanyForm(instance=company),
                     "address_form": CompanyAddressForm(instance=address),
                 }
@@ -120,4 +148,28 @@ class CompanyEditView(View):
                 form_company_address.save()
             return HttpResponseRedirect(
                         reverse('company_account')
+                    )
+
+
+class CompanyDeleteView(View):
+    """Edit Company View"""
+    def get(self, request):
+        if request.user.is_authenticated:
+            user = CustomUser.objects.select_related('company').get(id=request.user.id)
+            context = {
+                "user": user,
+                "company": user.company
+            }
+            return render(
+                request,
+                'company/delete_company.html',
+                context
+                )
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            user = CustomUser.objects.get(id=request.user.id)
+            user.delete()
+            return HttpResponseRedirect(
+                        reverse('home')
                     )
