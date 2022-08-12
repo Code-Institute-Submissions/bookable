01f22d03239a211acc262975948719c1ad5edcf5
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-12 14:10:30 +0200

    Refactor company signup form

--- company/forms.py
+++ company/forms.py
@@ -1,30 +1,8 @@
-from allauth.account.forms import SignupForm
 from django import forms
 from baseapp.models import Address, Company
 
 
-class CompanySignupForm(SignupForm):
-    """Extend Allauth Signup Form"""
-    first_name = forms.CharField(
-        max_length=30,
-        label = 'First Name',
-        widget=forms.TextInput(attrs={'placeholder': 'First Name'})
-    )
-    last_name = forms.CharField(
-        max_length=30,
-        label = 'Last Name',
-        widget=forms.TextInput(attrs={'placeholder': 'Last Name'})
-    )
-
-    def save(self, request):
-        user = super(CompanySignupForm, self).save(request)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
-        return user
-
-
-class NewCompanyForm(forms.ModelForm):
+class CompanyForm(forms.ModelForm):
     """New Company Form"""
     class Meta:
         model = Company
