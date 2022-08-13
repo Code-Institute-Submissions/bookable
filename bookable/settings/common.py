415ed66bd114684ce07795898dec97a5c5167023
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-13 19:05:38 +0200

    Add cloudinary needed imports

--- bookable/settings/common.py
+++ bookable/settings/common.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
