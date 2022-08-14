0bf589c5cd275f6e8c4b4625e50ef2b7a6638268
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-14 12:51:18 +0200

    Refactor company form to separate new company vs update company

--- company/forms.py
+++ company/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from cloudinary.forms import CloudinaryJsFileField
 from baseapp.models import Address, Company
 
 
@@ -7,7 +6,6 @@ class CompanyForm(forms.ModelForm):
     """New Company Form"""
     class Meta:
         model = Company
-        brand_image = CloudinaryJsFileField()
         fields = (
             'brand_image',
             'company_name',
@@ -19,6 +17,35 @@ class CompanyForm(forms.ModelForm):
             'category',
             )
 
+
+class CompanyEditForm(forms.ModelForm):
+    """Edit Company Form"""
+    user_id = forms.CharField()
+    street = forms.CharField()
+    city = forms.CharField()
+
+    class Meta:
+        model = Company
+        fields = (
+            'brand_image',
+            'company_name',
+            'phone',
+            'street',
+            'city',
+            'google_map',
+            'description',
+            'website',
+            'spots',
+            'category',
+            'user_id',
+            )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["company_name"].disabled = True
+        self.fields["company_name"].required = False
+        self.fields["user_id"].disabled = True
+        self.fields["user_id"].required = False
 
 
 class CompanyAddressForm(forms.ModelForm):
