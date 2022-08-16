214f0616a5286b8125d125d7a5b25e8d297a9d33
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-16 08:23:36 +0200

    Fix dependency filename

--- baseapp/migrations/0003_add_ordering_to_category_add_website_max_length.py
+++ baseapp/migrations/0003_add_ordering_to_category_add_website_max_length.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('baseapp', '0002_add_address_website_field_create_image_model'),
+        ('baseapp', '0002_add_website_field'),
     ]
 
     operations = [
