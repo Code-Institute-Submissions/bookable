6a53f395b951ac40142a5a175f0dc60630286d1e
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-17 06:21:15 +0200

    Rename cloudinary image delete signal function

--- baseapp/signals.py
+++ baseapp/signals.py
@@ -1,11 +1,12 @@
 """Baseapp Signals"""
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 import cloudinary
 from .models import Company
 
-@receiver(pre_delete, sender=Company)
-def company_brand_image_delete(sender, instance, **kwargs):
+
+@receiver(post_delete, sender=Company)
+def brand_image_delete_cloudinary(sender, instance, **kwargs):
     """Signal to delete company brand image
        when company object is deleted"""
     cloudinary.uploader.destroy(instance.brand_image.public_id)
