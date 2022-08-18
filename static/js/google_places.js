77bb719d7f25b217ad2c58a364eaa00d1086ea1d
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-18 13:18:08 +0200

    Rename of variables to be aligned with JS conventions

--- static/js/google_places.js
+++ static/js/google_places.js
@@ -1,13 +1,10 @@
-google_api_key = google_api_key.innerText.replace(/\\/g, '');
-google_api_key = google_api_key.replace(/\"/g, '');
+googleApiKey = google_api_key.innerText.replace(/\\/g, '');
+googleApiKey = googleApiKey.replace(/\"/g, '');
 
-// The code below is the same code done by Did Coding's tutorial with minor changes
+// The $.getScript() below is from Did Coding's tutorial
 // https://www.youtube.com/watch?v=-uxxRx2eZ70
 
-$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + google_api_key + "&libraries=places")
-.done(function( script, textStatus ) {
-  window.addEventListener('load', initAutocomplete)
-})
+$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + googleApiKey + "&libraries=places")
 
 // The code below is part of the sample code by google
 // https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform#try-sample
@@ -25,4 +22,4 @@ function initAutocomplete() {
   // address fields in the form.
 }
 
-window.initAutocomplete = initAutocomplete;
+window.addEventListener('load', initAutocomplete)
