742ec820324aa27cef275fae6a0afa91f048a30d
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-24 17:02:52 +0200

    Change customer phone field

--- baseapp/models.py
+++ baseapp/models.py
@@ -74,7 +74,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255)
+    phone = PhoneNumberField()
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
