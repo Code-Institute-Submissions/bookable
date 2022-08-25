fe2b67df4595e008b6b9661b868988d9cf4fa728
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-25 08:31:20 +0200

    Refactor email field & remove commented out code

--- booking/forms.py
+++ booking/forms.py
@@ -24,15 +24,12 @@ class BookingForm(forms.ModelForm):
     first_name = forms.CharField(max_length=255, required=True)
     last_name = forms.CharField(max_length=255,required=True)
     email = forms.EmailField(required=True)
-    # phone = forms.CharField(
-    #     label='Phone',
-    #     help_text='format: country code ie. +1 followed by 123-123-1234'
-    # )
     phone = PhoneNumberField(
         help_text='format: country code ie. +1 followed by 123-123-1234'
     )
 
     class Meta:
+        """Models fields to load"""
         model = Booking
         fields = (
             'spots',
@@ -46,17 +43,19 @@ class BookingForm(forms.ModelForm):
 
 class BookingCustomerDeleteForm(forms.ModelForm):
     """Booking Customer Delete Form"""
-    email = forms.CharField(
+    email = forms.EmailField(
         label='Your Email',
         widget=forms.TextInput(
             attrs={
                 "placeholder": "Your booking email"
             }
         ),
-        help_text='Please use the email you used when you made your booking.'
+        help_text='Please use the email you used when you made your booking.',
+        required=True
     )
 
     class Meta:
+        """Models fields to load"""
         model = Booking
         fields = (
             'email',
