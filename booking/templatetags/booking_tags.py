239a3e40e899bb6116ccc899fd76cb0a3716aec0
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-25 20:04:52 +0200

    Remove type tag from script tag

--- booking/templatetags/booking_tags.py
+++ booking/templatetags/booking_tags.py
@@ -9,14 +9,14 @@ register = template.Library()
 def booking_static():
     """Template tag to return booking static files"""
     static = (
-        '<script type="text/javascript" src="'
+        '<script src="'
         'https://res.cloudinary.com/daxqnc8yw/raw/upload/'
         'v1661260312/static/js/jquery.datetimepicker.full.min.349b0ca1b372.js'
         '"></script>'
-        '<script type="text/javascript" src="https://res.cloudinary.com/'
+        '<script src="https://res.cloudinary.com/'
         'daxqnc8yw/raw/upload/v1661260313/static/js/datetimepicker.'
         '1da7b1691d0f.js"></script>'
-        '<script type="text/javascript" src="https://res.cloudinary.com/'
+        '<script src="https://res.cloudinary.com/'
         'daxqnc8yw/raw/upload/v1661260312/static/js/google_map.'
         'b070891af348.js"></script>'
     )
