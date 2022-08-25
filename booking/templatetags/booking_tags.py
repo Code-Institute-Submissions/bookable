9fff4f1f9e75b55a80bd6a2638c016d15cadf2dd
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-25 10:22:40 +0200

    Fix indentation

--- booking/templatetags/booking_tags.py
+++ booking/templatetags/booking_tags.py
@@ -10,8 +10,8 @@ def booking_static():
     """Template tag to return booking static files"""
     static = (
         '<script type="text/javascript" src="'\
-            'https://res.cloudinary.com/daxqnc8yw/raw/upload/'\
-            'v1661260312/static/js/jquery.datetimepicker.full.min.349b0ca1b372.js"></script>'
+        'https://res.cloudinary.com/daxqnc8yw/raw/upload/'\
+        'v1661260312/static/js/jquery.datetimepicker.full.min.349b0ca1b372.js"></script>'
         '<script type="text/javascript" src="https://res.cloudinary.com/'\
         'daxqnc8yw/raw/upload/v1661260313/static/js/datetimepicker.1da7b1691d0f.js"></script>'
         '<script type="text/javascript" src="https://res.cloudinary.com/daxqnc8yw/raw/upload/'\
