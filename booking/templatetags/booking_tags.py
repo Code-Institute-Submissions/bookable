2bae87f4d2b5788bebc66fa4b0363e164d82e37e
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-26 12:05:19 +0200

    Change hardcoded JS script URL

--- booking/templatetags/booking_tags.py
+++ booking/templatetags/booking_tags.py
@@ -1,23 +1,28 @@
 """Booking Template Tags"""
 from django import template
+from django.templatetags.static import static
 from django.utils.html import format_html
 
 register = template.Library()
+
+DATE_TIME_PICKER_FULL = static('js/jquery.datetimepicker.full.min.js')
+DATE_TIME_PICKER = static('js/datetimepicker.js')
+GOOGLE_MAP = static('js/google_map.js')
 
 
 @register.simple_tag
 def booking_static():
     """Template tag to return booking static files"""
-    static = (
+    static_files = (
         '<script src="'
-        '{% static "js/jquery.datetimepicker.full.min.js" %}'
+        f'{DATE_TIME_PICKER_FULL}'
         '"></script>'
         '<script src="'
-        '{% static "js/datetimepicker.js" %}'
+        f'{DATE_TIME_PICKER}'
         '"></script>'
         '<script src="'
-        '{% static "js/google_map.js" %}'
+        f'{GOOGLE_MAP}'
         '"></script>'
     )
 
-    return format_html(static)
+    return format_html(static_files)
