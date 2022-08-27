6a6a95d12a33bc7eaba04486f10c82fd33a2f8ae
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-27 16:18:27 +0200

    Add pytest test companies file

--- baseapp/tests/test_companies.py
+++ baseapp/tests/test_companies.py
@@ -1,28 +1,35 @@
 """PyTest Companies"""
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, reverse, redirect
-from django.test import Client
+from django.shortcuts import render, reverse
+import pytest
+
+
+@pytest.fixture
+def create_company_info(test_client, user_client):
+    """Fixture for test company info"""
+    test_client.force_login(user_client)
+    def do_create_company_info(info):
+        return test_client.post('/company/add/', info)
+    return do_create_company_info
 
 
 class TestCreateCompany:
     """Test Get Company Pages"""
-    def test_if_user_is_anonymous_returns_302(self):
+    def test_if_user_is_anonymous_returns_302(self, test_client):
         """Test if anonymous"""
 
-        client = Client()
-        response = client.get('/company/add/')
+        response = test_client.get('/company/add/')
 
         assert response.status_code == 302
+        assert HttpResponseRedirect(reverse('home'))
 
 
-    def test_if_user_is_redirected_to_error_page(self, request, user_client):
+    def test_if_user_is_redirected_to_error_page(self, request, create_company_info):
         """Test if render to company does not exist"""
 
-        client = Client()
-        client.force_login(user_client)
-        response = client.post('/company/add/', {
-            "company_name": "company_test",
-            "address": "103 Greenwich Avenue, New York, NY, USA",
+        response = create_company_info({
+            "company_name": "company_error",
+            "address": "company_error_address",
             "phone": "cascascas",
             "website": "46546",
             "spots": "6",
@@ -32,12 +39,10 @@ class TestCreateCompany:
         assert render(request, 'company/form_company_not_valid.html')
 
 
-    def test_if_user_is_redirected_to_pending_page(self, request, user_client):
+    def test_if_user_is_redirected_to_pending_page(self, request, create_company_info):
         """Test if render to company pending"""
 
-        client = Client()
-        client.force_login(user_client)
-        response = client.post('/company/add/', {
+        response = create_company_info({
             "company_name": "company_test",
             "address": "103 Greenwich Avenue, New York, NY, USA",
             "phone": "+1 222-333-4444",
