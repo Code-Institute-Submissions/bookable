08b163099019882165e92337e6227815d8ae4a62
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-08-28 10:19:07 +0200

    Remove spaces from username if any on signup

--- core/forms.py
+++ core/forms.py
@@ -5,6 +5,9 @@ from django import forms
 
 class CompanySignupForm(SignupForm):
     """Extend Allauth Signup Form"""
+    username = forms.CharField(
+        help_text='No spaces is allowed, they are removed'
+    )
     first_name = forms.CharField(
         max_length=30,
         label = 'First Name',
@@ -18,7 +21,8 @@ class CompanySignupForm(SignupForm):
 
     def save(self, request):
         user = super(CompanySignupForm, self).save(request)
-        user.username = self.cleaned_data['username'].strip().lower()
+        user.username = self.cleaned_data['username']\
+            .strip().lower().replace(' ', '')
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.save()
